bb5aff07a89b133e472aeafeb28bdc3a7f402349
Nate Sloan <n8sloan@gmail.com>
2021-02-18 20:13:45 +0000

    added toLowerCase to solve error

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,10 +5,10 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName = '';
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = 'Who was the first woman in space? ';
+let question = 'Who was the first American woman in space? ';
 let correctAnswer = 'Sally Ride';
 let candidateAnswer = '';
-let questions = ["Who was the first American woman in space? ", "True or false: 5000 meters == 5 kilometers? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
 let correctAnswers = ['Sally Ride', 'true', '40', 'Trajectory', '3'];
 let candidateAnswers = [];
 
@@ -34,16 +34,7 @@ function gradeQuiz(candidateAnswers) {
 let totalGrade = 0;
 let questNum = 0
 for (i = 0; i < questions.length; i++) {
-  if (candidateAnswers[i] === correctAnswers[i]) {
-    totalGrade++;
-  console.log(`Question ${++questNum}: Correct!`);
-  } else if (candidateAnswers[i] === 'sally ride') {
-    totalGrade++;
-  console.log(`Question ${++questNum}: Correct!`);
-  } else if (candidateAnswers[i] === 'True') {
-    totalGrade++;
-  console.log(`Question ${++questNum}: Correct!`);
-  } else if (candidateAnswers[i] === 'trajectory') {
+  if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()) {
     totalGrade++;
   console.log(`Question ${++questNum}: Correct!`);
   } else {
